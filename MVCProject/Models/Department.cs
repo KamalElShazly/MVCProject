163f11b6968624ab92f44413d86307fb301b5810
KaMaL <KaMaL@KAMAL>
2017-04-08 18:41:20 +0200

    Wafaa

---
+++ MVCProject/Models/Department.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models
+{
+
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "The Name Must Be Between 2 and 50 characters")]
+        public string Name { get; set; }
+
+        [StringLength(50, MinimumLength = 20)]
+        public int Capacity { get; set; }
+
+      //  public List<Course> Courses { get; set; }
+
+        [ForeignKey("Ins")]
+        public int ManagerId { get; set; }
+       // public virtual Instructor Ins { get; set; }
+
+    }
+}
---
+++ MVCProject/Models/StudentExam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models
+{
+    public class StudentExam
+    {
+        [Key,ForeignKey("Std")]
+        public int StudentId { get; set; }
+
+        [Key, ForeignKey("Ex ")]
+        public int ExamId { get; set; }
+
+        [Required]
+        public float Grade { get; set; }
+
+      //  public virtual Student Std { get; set; }
+
+       // public virtual Exam Ex { get; set; }
+
+
+    }
+}
