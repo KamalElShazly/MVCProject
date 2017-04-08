ffdeb439cc9cdb9cf727cee1d27bbd1ecd25fc26
wafaa <fofa99441@yahoo.com>
2017-04-08 18:30:09 +0200

    wafaa

---
+++ MVCProject/Models/OfficialVacation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models
+{
+    public class OfficialVacation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50,MinimumLength =3,ErrorMessage ="The Name Must Be Between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ MVCProject/Models/Question.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Header { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        public List<string> Answer { get; set; }
+
+        [Required]
+        public string CorrectAnswer { get; set; }
+
+      //  public List<Exam> Exams { get; set; }
+    }
+}
